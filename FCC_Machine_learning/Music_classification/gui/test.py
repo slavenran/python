685597e5442f2ec739ff18df0879d469047a45a9
slavenr <ranisavljevicsan@gmail.com>
2021-10-10 19:32:02 +0200

    rearrangement of files

---
+++ FCC_Machine_learning/Music_classification/gui/test.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root = Tk();
+
+entry = Entry(root, width=50)
+entry.pack()
+
+title = Label(root, text="Upload a song to check its genre")
+
+def handleClick():
+    label = Label(root, text="Fill this with music prediction logic")
+    label.pack()
+
+button = Button(root, text="Click Me", command=handleClick, fg="blue", bg="red")
+
+button.pack()
+
+
+root.mainloop()
