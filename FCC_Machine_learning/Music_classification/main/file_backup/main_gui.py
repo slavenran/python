bd58415409e65df2d42bc286e9be8132ba7df930
slavenr <ranisavljevicsan@gmail.com>
2021-10-20 22:57:10 +0200

    tries to combine tkinter with asyncio to get rid of GUI freezing

--- FCC_Machine_learning/Music_classification/main/file_backup/main_gui.py
+++ FCC_Machine_learning/Music_classification/main/file_backup/main_gui.py
@@ -1,4 +1,3 @@
-import threading
 from tkinter import *
 from tkinter.ttk import *
 from tkinter.filedialog import askopenfile 
@@ -8,48 +7,37 @@ import asyncio
 
 loop = asyncio.get_event_loop()
 
+root = Tk()
+root.title("Music Genre Predictor")
+root.geometry("400x200")
+
 song_filepath = ''
 
-def _asyncio_thread(async_loop):
-    async_loop.run_until_complete(main())
+title = Label(root, text="Upload a song to check its genre")
 
-def do_tasks(async_loop):
-    threading.Thread(target=_asyncio_thread, args=(async_loop,)).start()
+buttonPredict = Button(root, text="Predict the genre", command=lambda:predict())
+button = Button(root, text="Choose a song", command=lambda:open_file())
+buttonAfter = Button(root, text="Choose another song", command=lambda:open_file())
 
-def gui(async_loop):
-    root = Tk()
+progressBar = Progressbar(root, orient=HORIZONTAL, length=100, mode='indeterminate')
 
-    root.title("Music Genre Predictor")
-    root.geometry("400x200")
+title.grid(row=0, column=0, columnspan=2)
+button.grid(row=2, column=0)
 
-
-    title = Label(root, text="Upload a song to check its genre")
-
-    progressBar = Progressbar(root, orient=HORIZONTAL, length=100, mode='indeterminate')
-    buttonPredict = Button(root, text="Predict the genre", command=lambda:predict(progressBar, root))
-    buttonAfter = Button(root, text="Choose another song", command=lambda:open_file(button, root, buttonPredict, buttonAfter))
-    button = Button(root, text="Choose a song", command=lambda:open_file(button, root, buttonPredict, buttonAfter))
-
-
-    title.grid(row=0, column=0, columnspan=2)
-    button.grid(row=2, column=0)
-
-    root.mainloop()
-
-def predict(progressBar, root):
+def predict():
     progressBar.grid(row=4, column=0, columnspan=2)
     progressBar.start(10)
-    asyncio.run(main(music_genre_prediction.predict(song_filepath), root))
+    asyncio.run(main(music_genre_prediction.predict(song_filepath)))
     progressBar.grid_remove()
 
-async def main(predict_fun, root):
+async def main(predict_fun):
     result = await prediction_string(predict_fun)
     Label(root, text=f"Your song is predicted to be of a {result} genre.").grid(row=3, column=0, columnspan=2)
 
 async def prediction_string(predict_fun):
     return predict_fun
 
-def open_file(button, root, buttonPredict, buttonAfter):
+def open_file():
     file_path = askopenfile(mode='r', filetypes=[("Audio Files", ".wav .ogg"),   ("All Files", "*.*")])
     if file_path is not None:
         button.grid_remove()
@@ -60,7 +48,7 @@ def open_file(button, root, buttonPredict, buttonAfter):
         buttonAfter.grid(row=2, column=1)
 
 
-def uploadFiles(root):
+def uploadFiles():
     pb1 = Progressbar(
         root, 
         orient=HORIZONTAL, 
@@ -75,6 +63,5 @@ def uploadFiles(root):
     pb1.destroy()
     Label(root, text='File Uploaded Successfully!', foreground='green').grid(row=1, columnspan=3, pady=10)
 
-if __name__ == '__main__':
-    async_loop = asyncio.get_event_loop()
-    gui(async_loop)
+
+root.mainloop()
