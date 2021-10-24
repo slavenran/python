7e9040b6027217c4d1843bc3b280b90844399809
slavenr <ranisavljevicsan@gmail.com>
2021-10-24 19:58:07 +0200

    fixes labels not destroying when they should

--- FCC_Machine_learning/Music_classification/main/main_gui.py
+++ FCC_Machine_learning/Music_classification/main/main_gui.py
@@ -1,8 +1,7 @@
 import threading
 from tkinter import *
 from tkinter.ttk import *
-from tkinter.filedialog import askopenfile 
-import time
+from tkinter.filedialog import askopenfile
 import music_genre_prediction
 import asyncio
 
@@ -27,11 +26,13 @@ async def async_predict(predict_fun, root):
 async def prediction_string(predict_fun):
     return predict_fun
 
-def open_file(button, root, buttonPredict, buttonAfter):
+def open_file(button, buttonPredict, buttonAfter, successMessage):
     file_path = askopenfile(mode='r', filetypes=[("Audio Files", ".wav .ogg"),   ("All Files", "*.*")])
     if file_path is not None:
         button.grid_remove()
-        Label(root, text='File Uploaded Successfully!', foreground='green').grid(row=1, columnspan=3, pady=10)
+        file_name_short = file_path.name.split('/')[-1]
+        successMessage.configure(text=f'You uploaded {file_name_short} successfully!')
+        successMessage.grid(row=1, columnspan=3, pady=10)
         global song_filepath 
         song_filepath = file_path.name
         buttonPredict.grid(row=2, column=0)
@@ -41,17 +42,19 @@ def open_file(button, root, buttonPredict, buttonAfter):
 def gui(async_loop):
     root = Tk()
 
+    # Basic window options
     root.title("Music Genre Predictor")
     root.geometry("400x200")
 
-
+    # Components initialization
     title = Label(root, text="Upload a song to check its genre")
+    successMessage = Label(root, text='File uploaded successfully', foreground='green')
     progressBar = Progressbar(root, orient=HORIZONTAL, length=100, mode='indeterminate')
     buttonPredict = Button(root, text="Predict the genre", command=lambda:do_prediction(async_loop, progressBar, root))
-    buttonAfter = Button(root, text="Choose another song", command=lambda:open_file(button, root, buttonPredict, buttonAfter))
-    button = Button(root, text="Choose a song", command=lambda:open_file(button, root, buttonPredict, buttonAfter))
+    buttonAfter = Button(root, text="Choose another song", command=lambda:open_file(button, buttonPredict, buttonAfter, successMessage))
+    button = Button(root, text="Choose a song", command=lambda:open_file(button, buttonPredict, buttonAfter, successMessage))
 
-
+    # Components setting
     title.grid(row=0, column=0, columnspan=2)
     button.grid(row=2, column=0)
 
