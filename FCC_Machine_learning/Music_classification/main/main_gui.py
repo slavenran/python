104637e16944957a1cc0ff42a3d1d2813b534921
slavenr <ranisavljevicsan@gmail.com>
2021-10-25 19:35:46 +0200

    fixes centering of elements inside of gui

--- FCC_Machine_learning/Music_classification/main/main_gui.py
+++ FCC_Machine_learning/Music_classification/main/main_gui.py
@@ -7,24 +7,28 @@ import asyncio
 
 song_filepath = ''
 
-def do_prediction(async_loop, progressBar, predictionMessage):
-    threading.Thread(target=_predict_thread, args=(async_loop, progressBar, predictionMessage)).start()
+def do_prediction(async_loop, progressBar, predictionMessage, buttonPredict, buttonAfter):
+    threading.Thread(target=_predict_thread, args=(async_loop, progressBar, predictionMessage, buttonPredict, buttonAfter)).start()
 
-def _predict_thread(async_loop, progressBar, predictionMessage):
-    async_loop.run_until_complete(predict_main(progressBar, predictionMessage))
+def _predict_thread(async_loop, progressBar, predictionMessage, buttonPredict, buttonAfter):
+    async_loop.run_until_complete(predict_main(progressBar, predictionMessage, buttonPredict, buttonAfter))
 
-def predict_main(progressBar, predictionMessage):
-    progressBar.grid(row=4, column=0, columnspan=2)
+def predict_main(progressBar, predictionMessage, buttonPredict, buttonAfter):
+    buttonPredict["state"] = "disabled"
+    buttonAfter["state"] = "disabled"
+    progressBar.grid(row=4, column=1, columnspan=2)
     progressBar.start(10)
     
     asyncio.run(async_predict(music_genre_prediction.predict(song_filepath), predictionMessage))
 
+    buttonPredict["state"] = "enable"
+    buttonAfter["state"] = "enable"
     progressBar.grid_remove()
 
 async def async_predict(predict_fun, predictionMessage):
     result = await prediction_string(predict_fun)
     predictionMessage.configure(text=f"Your song is predicted to be of a {result} genre.")
-    predictionMessage.grid(row=3, column=0, columnspan=2)
+    predictionMessage.grid(row=3, column=1, columnspan=2)
 
 async def prediction_string(predict_fun):
     return predict_fun
@@ -39,13 +43,13 @@ def open_file(button, buttonPredict, buttonAfter, successMessage, predictionMess
 
         file_name_short = file_path.name.split('/')[-1]
         successMessage.configure(text=f'You uploaded {file_name_short} successfully!')
-        successMessage.grid(row=1, columnspan=3, pady=10)
+        successMessage.grid(row=1, column=1, columnspan=2, pady=10)
 
         global song_filepath 
         song_filepath = file_path.name
 
-        buttonPredict.grid(row=2, column=0)
-        buttonAfter.grid(row=2, column=1)
+        buttonPredict.grid(row=2, column=1, pady=20)
+        buttonAfter.grid(row=2, column=2, pady=20)
 
 # main funtion
 def gui(async_loop):
@@ -59,15 +63,17 @@ def gui(async_loop):
     title = Label(root, text="Upload a song to check its genre")
     successMessage = Label(root, text='', foreground='green')
     predictionMessage = Label(root, text='')
-    progressBar = Progressbar(root, orient=HORIZONTAL, length=100, mode='indeterminate')
-    buttonPredict = Button(root, text="Predict the genre", command=lambda:do_prediction(async_loop, progressBar, predictionMessage))
+    progressBar = Progressbar(root, orient=HORIZONTAL, length=200, mode='indeterminate')
+    buttonPredict = Button(root, text="Predict the genre", command=lambda:do_prediction(async_loop, progressBar, predictionMessage, buttonPredict, buttonAfter))
     buttonAfter = Button(root, text="Choose another song", command=lambda:open_file(button, buttonPredict, buttonAfter, successMessage, predictionMessage))
     button = Button(root, text="Choose a song", command=lambda:open_file(button, buttonPredict, buttonAfter, successMessage))
 
     # Components setting
-    title.grid(row=0, column=0, columnspan=2)
-    button.grid(row=2, column=0)
+    title.grid(row=0, column=1, columnspan=2, pady=10)
+    button.grid(row=2, column=1, columnspan=2)
 
+    # root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure((0, 3), weight=1)
     root.mainloop()
 
 if __name__ == '__main__':
